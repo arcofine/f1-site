6c841c248bd3aeaeae50436826849bb6a604e71e
Armando Neto <armandoneto75@yahoo.ca>
2023-02-10 10:23:06 -0500

    BUG: fix Http/Https

--- pages/Standings/Driver.jsx
+++ pages/Standings/Driver.jsx
@@ -16,7 +16,7 @@ export default function Driver({championship}) {
                                    <Drivers data={championship} />
                                    <div style={{display: "block", overflow: "hidden", width: "100%", height: "70vw"}}>
                                           <Iframe
-                                                 url="http://thirtheen-home.myddns.me:8000/championships/championship?cid=2"
+                                                 url="/championships/championship?cid=2"
                                                  width="100%"
                                                  height="100%"
                                                  id=""
