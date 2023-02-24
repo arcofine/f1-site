95f9d0754da5d4c07b106eea8a5af34930ca24d7
Armando Neto <armandoneto75@yahoo.ca>
2023-02-24 13:12:57 -0500

    Added: error serveur handling for Latest result section

--- components/Cards/CardLineChart.js
+++ components/Cards/CardLineChart.js
@@ -1,8 +1,22 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import Iframe from "react-iframe";
 import Chart from "chart.js";
+// import console from "console";
 
 export default function CardLineChart() {
+       const [data, setData] = useState();
+
+       useEffect(() => {
+              const fetchData = async () => {
+                     // try {
+                     const testURL = await fetch("/championships/championship?cid=2").then((res) => {
+                            return res.status === 500 ? setData(false) : setData(true);
+                     });
+              };
+
+              fetchData();
+       }, []);
+
        return (
               <>
                      <div className="relative flex flex-col min-w-0 break-words w-full mb-0 shadow-lg rounded bg-blueGray-700">
@@ -18,28 +32,59 @@ export default function CardLineChart() {
                             </div>
                             <div className="p-4 flex-auto">
                                    {/* Chart */}
+                                   {data ? (
+                                          <div
+                                                 className="relative"
+                                                 style={{
+                                                        display: "block",
+                                                        overflow: "hidden",
+                                                        width: "100%",
+                                                        height: "900px",
+                                                 }}>
+                                                 <Iframe
+                                                        url="/championships/championship?cid=2"
+                                                        width="100%"
+                                                        height="100%"
+                                                        id=""
+                                                        className="xl:-mt-11  md\:-mt-11 md:-mt-13 sm:-mt-13 xs:-mt-19"
+                                                        display="block"
+                                                        position="relative"
+                                                        overflow="visible"
+                                                        scrolling="yes"
+                                                        // styles={{marginTop: "-180px"}}
+                                                 />
+                                          </div>
+                                   ) : (
+                                          <>
+                                                 <div className="relative flex flex-col min-w-0 break-words w-full mb-0 shadow-lg rounded bg-orange-500 mt-8 mb-5">
+                                                        <div className="rounded-t mb-0 px-4 py-3 pb-0 bg-transparent">
+                                                               <div className="flex flex-wrap items-center">
+                                                                      <div className="relative w-full max-w-full flex-grow flex-1">
+                                                                             <h6 className="uppercase text-blueGray-800 mb-0 text-center text-xs font-semibold">
+                                                                                    Serveur en manutention
+                                                                             </h6>
+                                                                      </div>
+                                                               </div>
+                                                        </div>
+                                                        <div className="p-1 flex-auto">
+                                                               {/* Chart */}
 
-                                   <div
-                                          className="relative"
-                                          style={{
-                                                 display: "block",
-                                                 overflow: "hidden",
-                                                 width: "100%",
-                                                 height: "900px",
-                                          }}>
-                                          <Iframe
-                                                 url="/championships/championship?cid=2"
-                                                 width="100%"
-                                                 height="100%"
-                                                 id=""
-                                                 className="xl:-mt-11  md\:-mt-11 md:-mt-13 sm:-mt-13 xs:-mt-19"
-                                                 display="block"
-                                                 position="relative"
-                                                 overflow="visible"
-                                                 scrolling="yes"
-                                                 // styles={{marginTop: "-180px"}}
-                                          />
-                                   </div>
+                                                               <div
+                                                                      className="relative"
+                                                                      style={{
+                                                                             display: "block",
+                                                                             overflow: "hidden",
+                                                                             width: "100%",
+                                                                             height: "50px",
+                                                                      }}>
+                                                                      <h4 className="mt-0 text-3xl leading-relaxed text-center text-blueGray-100">
+                                                                             Nous serons de retour dans quelques minutes
+                                                                      </h4>
+                                                               </div>
+                                                        </div>
+                                                 </div>
+                                          </>
+                                   )}
                             </div>
                      </div>
               </>
