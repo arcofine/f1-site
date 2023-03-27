462abc8e4c45ed882cf150fc8f2938cf88b8b635
jfChampagne13 <125072873+jfChampagne13@users.noreply.github.com>
2023-03-27 16:16:56 -0400

    Update Footer.js

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
                                                  points="2560 0 2560 100 0 100"></polygon>
                                    </svg>
                             </div>
-                            <div className="container mx-auto px-4" style="padding-top:30px;">
+                            <div className="container mx-auto px-4">
                                    <div className="flex flex-wrap items-center justify-center">
                                           <div className="flex flex-col mx-auto items-center justify-center">
                                                  <h2 className="text-md text-blueGray-500 font-semibold mb-2">
