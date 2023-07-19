04b68234fd661a71a5d30fd6c0fd48cad28d3e1c
Armando Neto <armandoneto75@yahoo.ca>
2023-07-19 14:06:03 -0400

    bug fix -rewrite

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 module.exports = {
        async rewrites() {
-              console.log("Rewrites called");
-              return process.env.NODE_ENV === "development"
-                     ? [
-                              {
-                                     source: "/:path*",
-                                     destination: "http://thirtheen-home.myddns.me:8802/:path*",
-                              },
-                              //   {
-                              //          source: "/dashboardGt/:path*",
-                              //          destination: "http://thirtheen-home.myddns.me:8804/:path*",
-                              //   },
-                       ]
-                     : [];
+              return [
+                     {
+                            source: "/:path*",
+                            destination: "http://thirtheen-home.myddns.me:8802/:path*",
+                     },
+                     //   {
+                     //          source: "/dashboardGt/:path*",
+                     //          destination: "http://thirtheen-home.myddns.me:8804/:path*",
+                     //   },
+              ];
        },
 };
