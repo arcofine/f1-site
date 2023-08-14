8beaea07bdc70eeb37cdcff22e547dad41da6e3a
jfChampagne13 <125072873+jfChampagne13@users.noreply.github.com>
2023-08-14 11:12:22 -0400

    Update Footer.js
    swolotech white

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -67,7 +67,7 @@ export default function Footer() {
                                                                title=""
                                                                className="hover:text-blueGray-500 text-blueGray-700 w-1/4 xs:w-1/3 px-3 flex items-center text-xs uppercase font-bold"
                                                                rel="noreferrer">
-                                                               <img alt="" src="/img/solotech.png" />
+                                                               <img alt="" src="/img/solotech-white.png" />
                                                         </a>
                                                         <a
                                                                href="https://www.simracinghub.com"
