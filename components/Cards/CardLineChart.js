2e94cdfc3fd94d095e5f7be63c505150994d4205
jfChampagne13 <125072873+jfChampagne13@users.noreply.github.com>
2023-08-14 10:31:01 -0400

    Update CardLineChart.js
    change cid=1 to 3 for season 2

--- components/Cards/CardLineChart.js
+++ components/Cards/CardLineChart.js
@@ -20,7 +20,7 @@ export default function CardLineChart({mode}) {
        useEffect(() => {
               const fetchData = async () => {
                      try {
-                            const testURL = await fetch("/championships/championship?cid=1");
+                            const testURL = await fetch("/championships/championship?cid=3");
                             setData(testURL.status);
                             console.log(testURL.status);
                             return testURL;
