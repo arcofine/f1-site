99ea08e6abc9a9436285b0744e8f24a1c82f95bb
Armando Neto <armandoneto75@yahoo.ca>
2024-04-06 20:11:52 -0400

    Review homepage

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -16,8 +16,8 @@ export default function Navbar(props) {
                      <Script src="/donate.js"></Script>
                      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black shadow">
                             <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                                   <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                                          <Link href="/" className=" inline-block mr-4 lg:hidden sm:hidden">
+                                   <div className="w-full relative flex justify-between  lg:justify-start">
+                                          <Link href="/" className="relative logo-50 inline-block mr-4 lg:hidden md:hidden">
                                                  <img
                                                         className="absolute top-0"
                                                         src="/img/logoNoir.png"
@@ -25,7 +25,7 @@ export default function Navbar(props) {
                                                         width="50"
                                                  />
                                           </Link>
-                                          <Link href="/" className="xs:hidden  inline-block mr-4">
+                                          <Link href="/" className="relative logo-90 xs:hidden sm:hidden inline-block mr-4">
                                                  <img
                                                         className="absolute top-0"
                                                         src="/img/logoNoir.png"
@@ -116,82 +116,90 @@ export default function Navbar(props) {
                                                                </span>
                                                         </a>
                                                  </li>
-                                                 {/* <li className="flex items-center">
-                                                        <a
-                                                               href="https://www.simracinghub.com"
-                                                               title=""
-                                                               className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">
-                                                               <img
-                                                                      alt=""
-                                                                      width="100"
-                                                                      src="https://www.simracinghub.com/images/SimRacingHub_2018_banner.png"
-                                                               />
-                                                        </a>
-                                                 </li> */}
-                                                 {/* <li>
-                                                        <p className="flex-auto ml-0">
-                                                               <span
-                                                                      className="text-blueGray-200
-                                                                                                  text-center
-                                                                                                  inline-flex
-                                                                                                  items-center
-                                                                                                  justify-center
-                                                                                                  w-1
-                                                                                                  px-12
-                                                                                                  h-6
-                                                                                                  mb-0
-
-                                                                                                  mt-3
-                                                                                                  shadow-lg
-                                                                                                  rounded-full
-                                                                                                  text-sm
-                                                                                                  font-bold
-                                                                                                  bg-red-500
-                                                                                                 ">
-                                                                      BETA
-                                                               </span>
-                                                        </p>
-                                                 </li> */}
-                                                 {/* <li className="flex items-center">
-                <button
-                  className="bg-blueGray-700 text-white active:bg-blueGray-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  <i className="fas fa-arrow-alt-circle-down"></i> Download
-                </button>
-              </li> */}
                                           </ul>
                                    </div>
                             </div>
                      </nav>
-                     <nav className="lg:top-68-px xs:top-56-px sm:top-56-px md:top-56-px  fixed z-40 w-full flex flex-wrap items-end justify-end xs:px-6 px-12 py-1 navbar-expand-lg bg-gray-f1 shadow">
-                            <div className={" top-0 -mt-4 py-2 mx-4 xs:hidden "}>
-                                   <form action="https://www.paypal.com/donate" method="post" target="_top">
-                                          <input type="hidden" name="hosted_button_id" value="B2DJ9MA4VQMPJ" />
-                                          <input
-                                                 type="image"
-                                                 src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-                                                 border="0"
-                                                 name="submit"
-                                                 title="PayPal - The safer, easier way to pay online!"
-                                                 alt="Donate with PayPal button"
-                                          />
-                                          <img
-                                                 alt=""
-                                                 border="0"
-                                                 src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
-                                                 width="1"
-                                                 height="1"
-                                          />
-                                   </form>
-                            </div>
-                            <div
-                                   id="lr-servers"
-                                   orientation="horizontal"
-                                   theme="dark"
-                                   className={
-                                          "relative flex flex-col min-w-0 break-words w-10/12 mb-6 shadow-lg rounded "
-                                   }></div>
+                     <nav className="lg:top-68-px xs:top-75-px sm:top-75-px md:top-75-px  fixed z-40 w-full flex flex-col flex-wrap items-end justify-end xs:px-6 px-12 py-1 navbar-expand-lg bg-gray-f1 shadow">
+                     {/* <div className="flex flex-col mx-auto items-center justify-center mt-2 mb-2">
+                                                 <form
+                                                        action="https://www.paypal.com/donate"
+                                                        method="post"
+                                                        target="_top">
+                                                        <input
+                                                               type="hidden"
+                                                               name="hosted_button_id"
+                                                               value="B2DJ9MA4VQMPJ"
+                                                        />
+                                                        <input
+                                                               type="image"
+                                                               src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                                                               border="0"
+                                                               name="submit"
+                                                               title="PayPal - The safer, easier way to pay online!"
+                                                               alt="Donate with PayPal button"
+                                                        />
+                                                        <img
+                                                               alt=""
+                                                               border="0"
+                                                               src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+                                                               width="1"
+                                                               height="1"
+                                                        />
+                                                 </form>
+                                          </div> */}
+                     <div className="flex flex-col mx-auto items-center justify-center mt-2 mb-2">
+                                          <a
+                            className="text-blueGray-300
+                                          text-center
+                                          inline-flex
+                                          items-center
+                                          justify-center
+                                          w-full
+                                          px-12
+                                          h-14
+                                          mb-0
+                                          shadow-lg
+                                          text-base
+                                          font-bold
+                                          border-2
+                                          hover:text-blueGray-500
+                                          roboto
+                                          "
+                            href="https://discord.gg/FHuMrCcTCS"
+                            target="_blank"
+                            rel="noreferrer">
+                                   CLIQUEZ ICI POUR JOINDRE NOTRE COMMUNAUTÉ
+                     </a>
+                                   </div>
+                            {/* <div className={"top-0 -mt-4 py-2 mx-4"}>
+                            <form
+                                                        action="https://www.paypal.com/donate"
+                                                        method="post"
+                                                        target="_top">
+                                                        <input
+                                                               type="hidden"
+                                                               name="hosted_button_id"
+                                                               value="B2DJ9MA4VQMPJ"
+                                                        />
+                                                        <input
+                                                               type="image"
+                                                               src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                                                               border="0"
+                                                               name="submit"
+                                                               title="PayPal - The safer, easier way to pay online!"
+                                                               alt="Donate with PayPal button"
+                                                        />
+                                                        <img
+                                                               alt=""
+                                                               border="0"
+                                                               src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+                                                               width="1"
+                                                               height="1"
+                                                        />
+                                                 </form>
+                            </div> */}
+                            
                      </nav>
               </>
        );
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,24 @@ export default function Index() {
               // access to player in all event handlers via event.target
               e.target.playVideo();
        };
+       const _onReady2 = (e) => {
+              // access to player in all event handlers via event.target
+              e.target.playVideo();
+       };
        const unique_id = new Date().getTime();
        const opts = {
               height: "540",
+              width: "100%",
+              playerVars: {
+                     autoplay: 1,
+                     loop: 1,
+                     mute: 1,
+                     controls: 0,
+                     modestbranding: 1,
+              },
+       };
+       const optsSmall = {
+              height: "250px",
               width: "100%",
               playerVars: {
                      autoplay: 1,
@@ -34,13 +49,13 @@ export default function Index() {
                             style={{
                                    backgroundImage: "url('/img/backgrounds/bg-02.jpg')",
                             }}
-                            className="relative pt-36 items-center flex flex-col xs:h-600-px h-800-px  bg-slate-900 bg-no-repeat bg-left bg-cover">
+                            className="relative xs:pt-32 sm:pt-32 items-center flex flex-col  bg-slate-900 bg-no-repeat bg-left bg-cover">
                             <div className="w-full h-full bg-blueGray-900 absolute opacity-80 z-0 top-0"></div>
 
-                            <div className=" container w-11/12 mx-auto items-center flex flex-wrap z-2">
+                            <div className=" container w-full mt-58 mx-auto items-center flex flex-wrap z-2">
                                    <div className=" mx-auto items-center absolute flex flex-wrap z-2"></div>
                                    <div className="w-full relative px-0 flex">
-                                          <div
+                                          {/* <div
                                                  className={
                                                         "absolute mx-auto top-0 -mt-13 left-38 lg:hidden md:hidden sm:hidden "
                                                  }>
@@ -69,29 +84,59 @@ export default function Index() {
                                                                height="1"
                                                         />
                                                  </form>
-                                          </div>
-                                          {/* <a
-                                                 href="https://docs.google.com/forms/d/e/1FAIpQLScI0ZmnPd8oMjOKmcLS4_aB_PdhfywaxBL4APgXhPQvAS_F0g/viewform?fbclid=IwAR14mMCbXUsS913ePFINQ4AQvltepyqW3c-MRkrJBIANSRag2bnx8LMZdrw"
-                                                 target="_blank"
-                                                 className="z-3 get-started text-center absolute w-full text-white font-bold px-2
-                                                  py-2 xs:py-0 outline-none focus:outline-none mr-1 mb-1 bg-red-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150">
-                                                 CLIQUE ICI POUR T'INSCRIRE MAINTENANT!
-                                          </a> */}
-                                          <div className="container mx-auto max-w-950-px ws:max-w-580-px ">
+                                          </div> */}
+                                      
+                                          <div className="container mx-auto max-w-950-px ws:max-w-580-px xs:hidden sm:hidden">
                                                  <YouTube
                                                         videoId="A7rFAimTgZk"
                                                         opts={opts}
+                                                        onReady={_onReady2}
+                                                        onEnd={_onReady2}
+                                                 />
+                                                 
+                                          </div>
+                                          <div className="container mx-auto max-w-950-px ws:max-w-580-px lg:hidden md:hidden ">
+                                                 <YouTube
+                                                        videoId="A7rFAimTgZk"
+                                                        opts={optsSmall}
                                                         onReady={_onReady}
                                                         onEnd={_onReady}
                                                  />
                                                  
                                           </div>
-                                          <div className=" mx-auto w-full items-center absolute flex h-500-px top-0 flex-wrap z-2">
-                                                 
+                                          <div className=" mx-auto w-full items-center absolute flex h-500-px bottom-0 flex-wrap z-2">
+                                         
                                           </div>
                                           
                                    </div>
-                                   <div className="flex flex-col mx-auto items-center justify-center mt-5">
+                                   <div className="flex flex-col mx-auto items-center justify-center mt-5 z-50">
+                                   <form
+                                                        action="https://www.paypal.com/donate"
+                                                        method="post"
+                                                        target="_top"
+                                                        style={{ position: "absolute", bottom: "-55px", left: "45.7%"
+                                                           }}>
+                                                        <input
+                                                               type="hidden"
+                                                               name="hosted_button_id"
+                                                               value="B2DJ9MA4VQMPJ"
+                                                        />
+                                                        <input
+                                                               type="image"
+                                                               src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                                                               border="0"
+                                                               name="submit"
+                                                               title="PayPal - The safer, easier way to pay online!"
+                                                               alt="Donate with PayPal button"
+                                                        />
+                                                        <img
+                                                               alt=""
+                                                               border="0"
+                                                               src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+                                                               width="1"
+                                                               height="1"
+                                                        />
+                                                 </form> 
                                           <a
                             className="text-blueGray-300
                                           text-center
