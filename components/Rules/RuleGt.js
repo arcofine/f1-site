f190b32a0aaac7691080896bb459f79541b51c10
jfChampagne13 <125072873+jfChampagne13@users.noreply.github.com>
2024-09-05 11:57:00 -0400

    Update RuleGt.js

--- components/Rules/RuleGt.js
+++ components/Rules/RuleGt.js
@@ -16,14 +16,14 @@ export default function RuleGt() {
                      />
                      <div className="flex justify-around" style={{marginTop: "-9vw", marginBottom: "70px"}}>
                             <a
-                                   href="https://drive.google.com/file/d/1NjUFu1N1CTr3lZ3R1dYAcyHnojm-KjAU/view?usp=drive_link"
+                                   href="https://drive.google.com/file/d/18bw7ATGTIsc8m3t-QbyAziUdppakfmFs/view?usp=drive_link"
                                    target="_blank"
                                    rel="noreferrer"
                                    className="button-54">
                                    Français
                             </a>
                             <a
-                                   href="https://drive.google.com/file/d/1-C1DjusulN6taLeeWTN4p5Il63fn5qR2/view?usp=drive_link"
+                                   href="https://drive.google.com/file/d/19g2IZzvAjnKpMUyigv5wDTRpg3grq77P/view?usp=drive_link"
                                    target="_blank"
                                    rel="noreferrer"
                                    className="button-54">
