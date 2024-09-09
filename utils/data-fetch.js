0ef9e2b47100ee5c72f955832f2ca858395f765b
jfChampagne13 <125072873+jfChampagne13@users.noreply.github.com>
2024-09-09 00:36:58 -0400

    Update data-fetch.js

--- utils/data-fetch.js
+++ utils/data-fetch.js
@@ -11,7 +11,7 @@ export async function getServerSideProps({
        defaultLocale,
 }) {
        console.log("Logging : " + res);
-       const data = await fetch("http://f1simgt3.ddns.net:8805/championships/export_standings_json.json?cid=1");
+       const data = await fetch("http://theovox-rdp.ddns.net:8803/championships/championship?cid=3");
        const championship = await data.json();
        return {props: {championship}};
 }
