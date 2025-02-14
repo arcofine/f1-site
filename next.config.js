140f175868dda4d8795ef464725533db3a4dcf20
Armando Neto <armandoneto75@yahoo.ca>
2025-02-14 11:44:30 -0500

    update live server

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
               return [
                      {
                             source: "/:path*",
-                            destination: "http://thirtheen-home.myddns.me:8802/:path*",
+                            destination: "http://206.172.240.123:8773/:path*",
                             
                      },
                      {
