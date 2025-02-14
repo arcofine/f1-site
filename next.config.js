9d1addeda4cc9aa74b6f6a58a2d7481955bec51a
Armando Neto <armandoneto75@yahoo.ca>
2025-02-14 11:57:50 -0500

    update empeor server

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
               return [
                      {
                             source: "/:path*",
-                            destination: "http://f1simgt3.ddns.net:8805/:path*",
+                            destination: "http://206.172.240.123:8774/:path*",
                             
                      },
                      {
