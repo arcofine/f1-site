b21b5065064bb80e230fdad465b55b4caf655350
Armando Neto <armandoneto75@yahoo.ca>
2025-05-04 15:48:42 -0400

    url update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
               return [
                      {
                             source: "/:path*",
-                          destination: "http://206.172.240.123:8888/:path*"
+                          destination: "http://142.120.193.175/:8888/:path*"
                      },
               ];
        },
